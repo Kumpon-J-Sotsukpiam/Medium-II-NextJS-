4060e6eafab6c1af2a07051da2468d29994f320d
kumpon J Sotsukpiam <kumpon_s@brainergy.digital>
2022-03-21 04:06:32 +0700

    update: content post page, update comment at post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,17 +1,41 @@
 import { GetStaticProps } from 'next'
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post as _Post } from '../../typing'
 import PortableText from 'react-portable-text';
-
+import { useForm, SubmitHandler } from 'react-hook-form'
 interface Props {
     post: _Post | any
 }
+interface IFormInput {
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
 
 export default function Post({ post }: Props) {
-    console.log(post);
-    
+    const [submitted, setSubmitted] = useState(false);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<IFormInput>();
+
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        fetch('/api/createComment', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        }).then(() => {
+            console.log(data);
+            setSubmitted(true);
+        }).catch((error) => {
+            console.log(error);
+            setSubmitted(false);
+        })
+    };
+
     return (
         <main>
             <Header />
@@ -37,10 +61,10 @@ export default function Post({ post }: Props) {
                         serializers={
                             {
                                 h1: (props: any) => (
-                                    <h1 className='text-2xl font-bold my-5' {...props}/>
+                                    <h1 className='text-2xl font-bold my-5' {...props} />
                                 ),
                                 h2: (props: any) => (
-                                    <h2 className='text-xl font-bold my-5' {...props}/>
+                                    <h2 className='text-xl font-bold my-5' {...props} />
                                 ),
                                 li: ({ children }: any) => (
                                     <h1 className='ml-4 list-disc'>{children}</h1>
@@ -55,6 +79,86 @@ export default function Post({ post }: Props) {
                     />
                 </div>
             </article>
+            <hr className='max-w-lg my-5 mx-auto border-yellow-500' />
+            {
+                submitted ? (
+                    <div className='flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto'>
+                        <h3 className='text-3xl font-bold'>Thank you for submitted your comment!</h3>
+                        <p>Once it has been approved, it will appear below!</p>
+                    </div>
+                ) : (
+                    <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col p-5 max-w-2xl mx-auto mb-10'>
+                        <h3 className='text-sm text-yellow-500'>Enjoyed this article?</h3>
+                        <h4 className='text-3xl font-bold'>Leave a commant below!</h4>
+                        <hr className='py-3 mt-2' />
+
+                        <input
+                            {...register("_id")}
+                            type='hidden'
+                            name='_id'
+                            value={post._id}
+                        />
+
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Name</span>
+                            <input
+                                {...register("name", { required: true })}
+                                className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring outline-none'
+                                type="text"
+                                placeholder='Kumpon J Sotsukpiam'
+                            />
+                        </label>
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Email</span>
+                            <input
+                                {...register("email", { required: true })}
+                                className='shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 focus:ring outline-none'
+                                type="text"
+                                placeholder='tetravaal.system@gmail.com'
+                            />
+                        </label>
+                        <label className='block mb-5'>
+                            <span className='text-gray-700'>Comment</span>
+                            <textarea
+                                {...register("comment", { required: true })}
+                                className='shadow border rounded py-2 px-3 mt-1 block w-full ring-yellow-500 focus:ring outline-none'
+                                placeholder=''
+                                rows={8}
+                            />
+                        </label>
+                        {/* errors will retunr when field validtion fails */}
+                        <div className='flex flex-col p-5'>
+                            {errors.name && (
+                                <span className='text-red-500'>- The Name Field is required</span>
+                            )}
+                            {errors.email && (
+                                <span className='text-red-500'>- The Email Field is required</span>
+                            )}
+                            {errors.comment && (
+                                <span className='text-red-500'>- The Comment Field is required</span>
+                            )}
+                        </div>
+                        <input
+                            className='shadow bg-yellow-500 text-white font-bold py-2 px-4 rounded cursor-pointer
+                        hover:bg-yellow-400 focus:shadow-outline focus:outline-none'
+                            type="submit"
+                        />
+                    </form>
+                )
+            }
+            <div className='flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2'>
+                <h3 className='text-4xl'>Comments</h3>
+                <hr className='pb-2' />
+                {
+                    post.comments.map((comment: any) => (
+                        <div key={comment._id}>
+                            <p>
+                                <span className='text-yellow-500'>{comment.name}</span>: {comment.comment}
+                            </p>
+                        </div>
+                    ))
+                }
+            </div>
         </main >
     )
 }
@@ -67,13 +171,11 @@ export const getStaticPaths = async () => {
         }
     }`;
     const posts = await sanityClient.fetch(query);
-
     const paths = posts.map((post: _Post) => ({
         params: {
             slug: post.slug.current
         }
     }))
-
     return {
         paths,
         fallback: 'blocking'
@@ -88,6 +190,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
           name,
           image
         },
+        'comments': *[_type == "comment" && post._ref == ^._id && approved == true],
         description,
         mainImage,
         slug,
--- typing.d.ts
+++ typing.d.ts
@@ -7,9 +7,25 @@ export interface Post {
     author: Author;
     body: Body[];
     categories: any[];
+    comments: Comment[];
     mainImage: MainImage;
     slug: Slug;
     title: string;
+}
+export interface Comment {
+    approved: boolean;
+    comment: string;
+    email: string;
+    name: string;
+    post: {
+        _ref: string;
+        _type: string;
+    },
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _type: string;
+    _updatedAt: string;
 }
 export interface Author {
     _ref: string;
